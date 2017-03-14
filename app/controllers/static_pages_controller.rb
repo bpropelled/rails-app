a8388c3d3d8572741e29863a60ef0cca60b357ea
bpropelled <abgquickbase@gmail.com>
2017-03-14 14:55:06 -0400

    added static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
-      render html: "home Page"
+#      render html: "home Page"
   end
 
   def help
-      render html: 'hepl page'
+#      render html: 'hepl page'
   end
+    
+    def about 
+    end
 end
